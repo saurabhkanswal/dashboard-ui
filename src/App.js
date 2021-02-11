de494e8bc1f55aef0089bee8e3133ac193be9b71
saurabhkanswal <saurabhkanswal8101@gmail.com>
2021-02-11 13:13:51 +0530

    navabar
    -> setup react boilerplate
    -> added navbar with react-router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home'
+import Post from './pages/Post'
+import User from './pages/User'
+import Sidepannel from './component/Sidepannel'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <Router>
+    <Sidepannel/>
+    <Switch>
+      <Route path='/' exact component={Home}/>
+      <Route path='/post' component={Post}/>
+      <Route path='/user' component={User}/>
+    </Switch>
+    </Router>
+    </>
+    );
 }
 
 export default App;
